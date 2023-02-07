9fe82c20c2655c65615c65207d1470d595e0893b
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-02-07 22:46:07 +0200

    delete index.js (code without refactoring)

--- src/index.js
+++ src/index.js
@@ -2,89 +2,153 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import PhotosApiService from './js/PhotoApiService';
+import LoadMoreBtn from './js/LoadMoreBtn';
+
 const refs = {
   form: document.getElementById('search-form'),
   gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
 };
 
-const URL_BASE = 'https://pixabay.com/api/';
-const KEY = '33393357-f6d954601800afd866273582d';
-const perPage = 40;
-let page = 1;
-let q = '';
+const photosApiService = new PhotosApiService();
+const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more', hidden: true });
 
 let gallery = new SimpleLightbox('.gallery a');
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.form.addEventListener('submit', onSearch);
+loadMoreBtn.btnEl.addEventListener('click', onLoadMore);
 
-function onLoadMore() {
-  page += 1;
-  const url = `${URL_BASE}?key=${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-  fetchPhotos(url);
+async function onSearch(e) {
+  e.preventDefault();
+
+  clearGallery();
+
+  loadMoreBtn.hide();
+
+  photosApiService.query = e.currentTarget.searchQuery.value;
+  photosApiService.resetPage();
+
+  try {
+    const photos = await photosApiService.fetchPhotos();
+
+    if (photos.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    Notify.success(`Hooray! We found ${photos.totalHits} images.`);
+
+    appendPhotosMarkup(createMarkup(photos.hits));
+
+    if (photosApiService.numberOfLoadedPhotos < photosApiService.totalHits) {
+      loadMoreBtn.show();
+      loadMoreBtn.enable();
+    }
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    e.target.reset();
+  }
+
+  //   photosApiService
+  //     .fetchPhotos()
+  //     .then(data => {
+  //       if (data.hits.length === 0) {
+  //         Notify.failure(
+  //           'Sorry, there are no images matching your search query. Please try again.'
+  //         );
+  //       } else {
+  //         Notify.info(`Hooray! We found ${data.totalHits} images.`);
+
+  //         appendPhotosMarkup(createMarkup(data.hits));
+
+  //         if (!onTheEndOfCollection()) {
+  //           loadMoreBtn.show();
+  //           loadMoreBtn.enable();
+  //         }
+  //       }
+  //     })
+  //     .then(makeSmoothScrollEffect);
+
+  //   e.currentTarget.reset();
 }
 
-function onFormSubmit() {
-  event.preventDefault();
-
+function clearGallery() {
   refs.gallery.innerHTML = '';
-
-  page = 1;
-  q = event.target.searchQuery.value;
-  const url = `${URL_BASE}?key=${KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-  fetchPhotos(url);
-  event.target.reset();
 }
 
-function fetchPhotos(url) {
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      if (data.hits.length === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        Notify.info(`Hooray! We found ${data.totalHits} images.`);
-        refs.gallery.insertAdjacentHTML('beforeend', renderMarkup(data.hits));
-        gallery.refresh();
-        refs.loadMoreBtn.classList.remove('hidden');
-      }
-    })
-    .finally(() => {
-      const { height: cardHeight } =
-        refs.gallery.firstElementChild.getBoundingClientRect();
+async function onLoadMore() {
+  if (photosApiService.numberOfLoadedPhotos >= photosApiService.totalHits) {
+    loadMoreBtn.hide();
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    return;
+  }
 
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
-      });
-    });
+  loadMoreBtn.disable();
+
+  try {
+    const photos = await photosApiService.fetchPhotos();
+
+    appendPhotosMarkup(createMarkup(photos.hits));
+    loadMoreBtn.enable();
+
+    makeSmoothScrollEffect();
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  //   photosApiService
+  //     .fetchPhotos()
+  //     .then(data => {
+  //       appendPhotosMarkup(createMarkup(data.hits));
+  //       loadMoreBtn.enable();
+  //     })
+  //     .then(makeSmoothScrollEffect);
 }
 
-function renderMarkup(photos) {
+function makeSmoothScrollEffect() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function appendPhotosMarkup(markup) {
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  gallery.refresh();
+}
+
+function createMarkup(photos) {
   return photos.reduce(
-    (acc, photo) =>
+    (
+      acc,
+      { largeImageURL, webformatURL, tags, likes, views, comments, downloads }
+    ) =>
       acc +
-      `<a class="photo-link" href="${photo.largeImageURL}"><div class="photo-card">
-  <img src="${photo.webformatURL}" class="photo"  alt="${photo.tags}" height="158px" loading="lazy"/>
+      `<a class="photo-link" href="${largeImageURL}"><div class="photo-card">
+  <img src="${webformatURL}" class="photo"  alt="${tags}" height="158px" loading="lazy"/>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
-      <span>${photo.likes}</span>
+      <span>${likes}</span>
     </p>
     <p class="info-item">
       <b>Views</b>
-      <span>${photo.views}</span>
+      <span>${views}</span>
     </p>
     <p class="info-item">
       <b>Comments</b>
-      <span>${photo.comments}</span>
+      <span>${comments}</span>
     </p>
     <p class="info-item">
       <b>Downloads</b>
-      <span>${photo.downloads}</span>
+      <span>${downloads}</span>
     </p>
   </div>
 </div></a>`,
